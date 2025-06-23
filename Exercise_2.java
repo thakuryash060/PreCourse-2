d8e91c80f8fd91b65c47e2aab85d0c2eacd6b59d
Yash Thakur <144375330+thakuryash060@users.noreply.github.com>
2025-06-23 18:58:02 +0530

    Update Exercise_2.java

--- Exercise_2.java
+++ Exercise_2.java
@@ -7,21 +7,35 @@ class QuickSort
        pivot and all greater elements to right 
        of pivot */
     void swap(int arr[],int i,int j){
-        //Your code here   
+       int temp = arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
     }
     
     int partition(int arr[], int low, int high) 
-    { 
-   	//Write code here for Partition and Swap 
-    } 
+    {
+     int index = low-1;
+     int pivot = arr[high];
+     for(int i=low;i<high;i++){
+         if(arr[i]<=pivot){
+             index++;
+             swap(arr,index,i);
+         }
+     }index++;
+        swap(arr,index,high);
+        return index;
+    }
     /* The main function that implements QuickSort() 
       arr[] --> Array to be sorted, 
       low  --> Starting index, 
       high  --> Ending index */
     void sort(int arr[], int low, int high) 
     {  
-            // Recursively sort elements before 
-            // partition and after partition 
+        if(low<high){
+            int pivot = partition(arr,low,high);
+            sort(arr,low,pivot-1);
+            sort(arr,pivot+1,high);
+        }
     } 
   
     /* A utility function to print array of size n */
